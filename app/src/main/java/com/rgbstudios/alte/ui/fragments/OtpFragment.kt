2f795af59b3e2c263a62d2f56d478f0e87146f5f
Oladayo <oladayobb@gmail.com>
2023-12-31 19:13:34 +0100

    phone number auth set up with reCAPTCHA disabled and using fictional phone number

--- app/src/main/java/com/rgbstudios/alte/ui/fragments/OtpFragment.kt
+++ app/src/main/java/com/rgbstudios/alte/ui/fragments/OtpFragment.kt
@@ -1,17 +1,41 @@
 package com.rgbstudios.alte.ui.fragments
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.FirebaseException
+import com.google.firebase.FirebaseTooManyRequestsException
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
 import com.rgbstudios.alte.R
+import com.rgbstudios.alte.data.remote.FirebaseAccess
 import com.rgbstudios.alte.databinding.FragmentOtpBinding
+import com.rgbstudios.alte.utils.ToastManager
+import com.rgbstudios.alte.viewmodel.AlteViewModel
+import java.util.concurrent.TimeUnit
 
 class OtpFragment : Fragment() {
 
     private lateinit var binding: FragmentOtpBinding
+    private lateinit var verificationId: String
+    private lateinit var resendingToken: PhoneAuthProvider.ForceResendingToken
+    private lateinit var fullPhoneNumber: String
+    private val sharedViewModel: AlteViewModel by activityViewModels()
+    private val toastManager = ToastManager()
+    private val firebase = FirebaseAccess()
+    private val auth = firebase.auth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,12 +47,184 @@ class OtpFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        addTextChangeListener()
+        resendOTPTvVisibility()
+
+        verificationId = sharedViewModel.verificationInfo.value!!.first
+        resendingToken = sharedViewModel.verificationInfo.value!!.second
+        fullPhoneNumber = sharedViewModel.verificationInfo.value!!.third
 
         binding.apply {
+            otpProgressBar.visibility = View.INVISIBLE
+
             verifyOTPBtn.setOnClickListener {
-                // Navigate to
-                findNavController().navigate(R.id.action_otpFragment_to_homeFragment)
+
+                handleOTPVerification(verificationId)
+            }
+
+            resendTextView.setOnClickListener {
+                resendOTPTvVisibility()
+                resendVerificationCode()
             }
         }
+    }
+
+    private fun handleOTPVerification(verificationId: String) {
+        binding.apply {
+            //collect otp from all the edit texts
+            val typedOTP =
+                (otpEditText1.text.toString() + otpEditText2.text.toString() + otpEditText3.text.toString()
+                        + otpEditText4.text.toString() + otpEditText5.text.toString() + otpEditText6.text.toString())
+
+            if (typedOTP.isNotEmpty()) {
+                if (typedOTP.length == 6) {
+                    val credential: PhoneAuthCredential = PhoneAuthProvider.getCredential(
+                        verificationId, typedOTP
+                    )
+                    otpProgressBar.visibility = View.VISIBLE
+                    signInWithPhoneAuthCredential(credential)
+                } else {
+                    Toast.makeText(requireContext(), "Please Enter Correct OTP", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            } else {
+                Toast.makeText(requireContext(), "Please Enter OTP", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    }
+
+    private fun resendOTPTvVisibility() {
+        binding.apply {
+            otpEditText1.setText("")
+            otpEditText2.setText("")
+            otpEditText3.setText("")
+            otpEditText4.setText("")
+            otpEditText5.setText("")
+            otpEditText6.setText("")
+            resendTextView.visibility = View.INVISIBLE
+            resendTextView.isEnabled = false
+
+            Handler(Looper.myLooper()!!).postDelayed(Runnable {
+                resendTextView.visibility = View.VISIBLE
+                resendTextView.isEnabled = true
+            }, 90000)
+        }
+    }
+
+    private fun resendVerificationCode() {
+        val options = PhoneAuthOptions.newBuilder(auth)
+            .setPhoneNumber(fullPhoneNumber)       // Phone number to verify
+            .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+            .setActivity(requireActivity())                 // Activity (for callback binding)
+            .setCallbacks(callbacks)
+            .setForceResendingToken(resendingToken)// OnVerificationStateChangedCallbacks
+            .build()
+        PhoneAuthProvider.verifyPhoneNumber(options)
+    }
+
+    private val callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+            // This callback will be invoked in two situations:
+            // 1 - Instant verification. In some cases the phone number can be instantly
+            //     verified without needing to send or enter a verification code.
+            // 2 - Auto-retrieval. On some devices Google Play services can automatically
+            //     detect the incoming verification SMS and perform verification without
+            //     user action.
+            signInWithPhoneAuthCredential(credential)
+        }
+
+        override fun onVerificationFailed(e: FirebaseException) {
+            // This callback is invoked in an invalid request for verification is made,
+            // for instance if the the phone number format is not valid.
+
+            if (e is FirebaseAuthInvalidCredentialsException) {
+                // Invalid request
+                Log.d("TAG", "onVerificationFailed: ${e.toString()}")
+            } else if (e is FirebaseTooManyRequestsException) {
+                // The SMS quota for the project has been exceeded
+                Log.d("TAG", "onVerificationFailed: ${e.toString()}")
+            }
+            binding.otpProgressBar.visibility = View.VISIBLE
+            // Show a message and update the UI
+        }
+
+        override fun onCodeSent(
+            verificationId: String,
+            token: PhoneAuthProvider.ForceResendingToken
+        ) {
+            // The SMS verification code has been sent to the provided phone number, we
+            // now need to ask the user to enter the code and then construct a credential
+            // by combining the code with a verification ID.
+            // Save verification ID and resending token so we can use them later
+            sharedViewModel.saveVerificationInfo(verificationId, token, fullPhoneNumber)
+        }
+    }
+
+    private fun addTextChangeListener() {
+        binding.apply {
+            otpEditText1.addTextChangedListener(EditTextWatcher(otpEditText1))
+            otpEditText2.addTextChangedListener(EditTextWatcher(otpEditText2))
+            otpEditText3.addTextChangedListener(EditTextWatcher(otpEditText3))
+            otpEditText4.addTextChangedListener(EditTextWatcher(otpEditText4))
+            otpEditText5.addTextChangedListener(EditTextWatcher(otpEditText5))
+            otpEditText6.addTextChangedListener(EditTextWatcher(otpEditText6))
+        }
+    }
+
+    inner class EditTextWatcher(private val view: View) : TextWatcher {
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+        }
+
+        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+        }
+
+        override fun afterTextChanged(p0: Editable?) {
+            binding.apply {
+
+                val text = p0.toString()
+                when (view.id) {
+                    R.id.otpEditText1 -> if (text.length == 1) otpEditText2.requestFocus()
+                    R.id.otpEditText2 -> if (text.length == 1) otpEditText3.requestFocus() else if (text.isEmpty()) otpEditText1.requestFocus()
+                    R.id.otpEditText3 -> if (text.length == 1) otpEditText4.requestFocus() else if (text.isEmpty()) otpEditText2.requestFocus()
+                    R.id.otpEditText4 -> if (text.length == 1) otpEditText5.requestFocus() else if (text.isEmpty()) otpEditText3.requestFocus()
+                    R.id.otpEditText5 -> if (text.length == 1) otpEditText6.requestFocus() else if (text.isEmpty()) otpEditText4.requestFocus()
+                    R.id.otpEditText6 -> if (text.isEmpty()) otpEditText5.requestFocus()
+
+                }
+            }
+        }
+
+    }
+
+    private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Toast.makeText(
+                        requireContext(),
+                        "Authenticate Successfully",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                    // Navigate
+                    findNavController().navigate(R.id.action_otpFragment_to_homeFragment)
+                } else {
+                    // Sign in failed, display a message and update the UI
+                    Log.d("TAG", "signInWithPhoneAuthCredential: ${task.exception.toString()}")
+                    if (task.exception is FirebaseAuthInvalidCredentialsException) {
+                        // The verification code entered was invalid
+                        toastManager.showShortToast(
+                            requireContext(),
+                            "Invalid phone number format."
+                        )
+                    }
+                    // Update UI
+                }
+            }
     }
 }
--- app/src/main/java/com/rgbstudios/alte/ui/fragments/SignInFragment.kt
+++ app/src/main/java/com/rgbstudios/alte/ui/fragments/SignInFragment.kt
@@ -1,34 +1,221 @@
 package com.rgbstudios.alte.ui.fragments
 
-import android.app.Activity.RESULT_OK
-import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
-import com.firebase.ui.auth.AuthUI
-import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.FirebaseException
+import com.google.firebase.FirebaseTooManyRequestsException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
 import com.rgbstudios.alte.R
 import com.rgbstudios.alte.databinding.FragmentSignInBinding
+import com.rgbstudios.alte.utils.CountryCodeProvider
+import com.rgbstudios.alte.utils.ToastManager
+import com.rgbstudios.alte.viewmodel.AlteViewModel
+import java.util.concurrent.TimeUnit
 
 class SignInFragment : Fragment() {
 
+    private val sharedViewModel: AlteViewModel by activityViewModels()
+
     private lateinit var binding: FragmentSignInBinding
+    private lateinit var fullPhoneNumber:String
+    private val countryCodeProvider = CountryCodeProvider()
+    private val toastManager = ToastManager()
+    private val auth = FirebaseAuth.getInstance()
+    private var verificationStarted = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentSignInBinding.inflate(inflater, container, false)
+        verificationStarted = savedInstanceState?.getBoolean("verificationStarted") ?: false
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.apply {
+            // Set up the Spinner (Dropdown)
+            val countryCodeList = countryCodeProvider.getCountryCodes()
+
+            val adapter =
+                ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, countryCodeList)
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            numberPrefixSpinner.adapter = adapter
+
+            // Set the default selection
+            val defaultSelection = countryCodeList.indexOf("+234")
+            numberPrefixSpinner.setSelection(defaultSelection)
+
+            sendOTPBtn.setOnClickListener {
+                val countryCode = numberPrefixSpinner.selectedItem as String
+                val phoneNumber = phoneEditTextNumber.text.toString()
+
+                if (phoneNumber.isNotBlank()) {
+                    fullPhoneNumber = "$countryCode$phoneNumber"
+
+                    progressBar.visibility = View.VISIBLE
+                    sendOTPBtn.text = ""
+
+                    // Initiate phone authentication
+                    initiatePhoneAuthentication()
+                } else {
+                    toastManager.showShortToast(requireContext(), "Please enter a phone number.")
+                }
+            }
+        }
     }
-    
+
+    private fun initiatePhoneAuthentication() {
+        // The test phone number and code should be whitelisted in the console.
+        val phoneNumber = "+16505554567"
+        val smsCode = "123456"
+
+        val firebaseAuthSettings = auth.firebaseAuthSettings
+
+        // Configure faking the auto-retrieval with the whitelisted numbers.
+        firebaseAuthSettings.setAutoRetrievedSmsCodeForPhoneNumber(phoneNumber, smsCode)
+
+        val options = PhoneAuthOptions.newBuilder(auth)
+            .setPhoneNumber(phoneNumber)
+            .setTimeout(30L, TimeUnit.SECONDS)
+            .setActivity(requireActivity())
+            .setCallbacks(object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+                    signInWithPhoneAuthCredential(credential)
+                    verificationStarted = false
+
+                    binding.progressBar.visibility = View.GONE
+                    binding.sendOTPBtn.text = getString(R.string.send)
+                }
+
+                override fun onVerificationFailed(e: FirebaseException) {
+                    // Handle verification failure
+                    if (e is FirebaseAuthInvalidCredentialsException) {
+                        toastManager.showShortToast(requireContext(), "Invalid phone number format.")
+                    } else if (e is FirebaseTooManyRequestsException) {
+                        toastManager.showShortToast(requireContext(), "SMS quota exceeded. Please try again later.")
+                    }
+                    verificationStarted = false
+                    binding.progressBar.visibility = View.GONE
+                    binding.sendOTPBtn.text = getString(R.string.send)
+                }
+
+                override fun onCodeSent(
+                    verificationId: String,
+                    token: PhoneAuthProvider.ForceResendingToken
+                ) {
+                    sharedViewModel.saveVerificationInfo(verificationId, token, fullPhoneNumber)
+                    binding.progressBar.visibility = View.GONE
+                    binding.sendOTPBtn.text = getString(R.string.send)
+
+                    // Navigate to OtpFragment
+                    navigateToOtpFragment()
+                }
+            })
+            .build()
+        PhoneAuthProvider.verifyPhoneNumber(options)
+
+        /*
+
+        toastManager.showLongToast(requireContext(), fullPhoneNumber)
+
+        val options = PhoneAuthOptions.newBuilder(auth)
+            .setPhoneNumber(fullPhoneNumber)
+            .setTimeout(60L, TimeUnit.SECONDS)
+            .setActivity(requireActivity())
+            .setCallbacks(callbacks)
+            .build()
+
+        PhoneAuthProvider.verifyPhoneNumber(options)
+        verificationStarted = true
+
+         */
+    }
+
+    private val callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+        override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+            signInWithPhoneAuthCredential(credential)
+            verificationStarted = false
+        }
+
+        override fun onVerificationFailed(e: FirebaseException) {
+            // Handle verification failure
+            if (e is FirebaseAuthInvalidCredentialsException) {
+                toastManager.showShortToast(requireContext(), "Invalid phone number format.")
+            } else if (e is FirebaseTooManyRequestsException) {
+                toastManager.showShortToast(requireContext(), "SMS quota exceeded. Please try again later.")
+            }
+            verificationStarted = false
+        }
+
+        override fun onCodeSent(
+            verificationId: String,
+            token: PhoneAuthProvider.ForceResendingToken
+        ) {
+            sharedViewModel.saveVerificationInfo(verificationId, token, fullPhoneNumber)
+
+            // Navigate to OtpFragment
+            navigateToOtpFragment()
+        }
+    }
+
+    private fun navigateToOtpFragment() {
+        findNavController().navigate(R.id.action_signInFragment_to_otpFragment)
+    }
+
+    private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Toast.makeText(requireContext() , "Authentication Successful!" , Toast.LENGTH_SHORT).show()
+                    // Navigate
+                    findNavController().navigate(R.id.action_signInFragment_to_homeFragment)
+                } else {
+                    // Sign in failed, display a message and update the UI
+                    Log.d("TAG", "signInWithPhoneAuthCredential: ${task.exception.toString()}")
+                    if (task.exception is FirebaseAuthInvalidCredentialsException) {
+                        // The verification code entered was invalid
+                    }
+                    // Update UI
+                }
+            }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        // Save the state of flags
+        outState.putBoolean("verificationStarted", verificationStarted)
+    }
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+
+        // Restore the state of flags
+        verificationStarted =
+            savedInstanceState?.getBoolean("verificationStarted") ?: false
+
+        binding.apply {
+            // Reset imageSample layout visibility
+            if (verificationStarted) {
+                initiatePhoneAuthentication()
+            }
+        }
+    }
+
 }
