829f5158e3de6a84c8d7eba422f946a043e326d6
Oladayo <oladayobb@gmail.com>
2024-01-17 19:07:54 +0100

    Peeps recyclerView set up, (like instagram stories and whatsApp statuses) and the fab button navigation to the AlteVerseFragment where user can connect with other users (RV still set up with dummy peeps)

--- app/src/main/java/com/rgbstudios/alte/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/rgbstudios/alte/ui/fragments/HomeFragment.kt
@@ -1,17 +1,39 @@
 package com.rgbstudios.alte.ui.fragments
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.airbnb.lottie.LottieAnimationView
+import android.widget.PopupMenu
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.rgbstudios.alte.R
+import com.rgbstudios.alte.data.remote.FirebaseAccess
+import com.rgbstudios.alte.data.repository.AlteRepository
 import com.rgbstudios.alte.databinding.FragmentHomeBinding
+import com.rgbstudios.alte.ui.adapters.PeepAdapter
+import com.rgbstudios.alte.utils.AvatarManager
+import com.rgbstudios.alte.utils.DialogManager
+import com.rgbstudios.alte.utils.ToastManager
+import com.rgbstudios.alte.viewmodel.AlteViewModel
+import com.rgbstudios.alte.viewmodel.AlteViewModelFactory
 
 class HomeFragment : Fragment() {
+    private val firebase = FirebaseAccess()
+
+    private val alteViewModel: AlteViewModel by viewModels {
+        AlteViewModelFactory(requireActivity().application, AlteRepository(firebase))
+    }
+
+    private val auth = firebase.auth
+    private val toastManager = ToastManager()
+    private val dialogManager = DialogManager()
+    private val avatarManager = AvatarManager()
     private lateinit var binding: FragmentHomeBinding
-    private lateinit var customBar: LottieAnimationView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,31 +46,104 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.apply {
-            customBar = lottieAnimationView
+
+            // Set up the planet adapter
+            val peepAdapter = PeepAdapter(requireContext(), alteViewModel)
+
+            // Set up the planetRecyclerView to scroll horizontally
+            peepRecyclerView.setHasFixedSize(true)
+            peepRecyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+            peepRecyclerView.adapter = peepAdapter
+
+            val peepList = avatarManager.defaultAvatarsList
+            val peeps = convertResourceIdsToBitmaps(peepList)
+
+            peepAdapter.updatePeeps(peeps)
+
             showAnim.setOnClickListener {
                 texty.visibility = View.GONE
+                texty2.visibility = View.GONE
                 showAnim.visibility = View.GONE
-                customBar.visibility = View.VISIBLE
+                lottieAnimationView.visibility = View.VISIBLE
                 closer.visibility = View.VISIBLE
             }
 
             closer.setOnClickListener {
                 texty.visibility = View.VISIBLE
+                texty2.visibility = View.VISIBLE
                 showAnim.visibility = View.VISIBLE
 
-                customBar.visibility = View.GONE
+                lottieAnimationView.visibility = View.GONE
                 closer.visibility = View.GONE
+            }
+
+            moreOptions.setOnClickListener { view ->
+                showOverflowMenu(view)
+            }
+
+            fab.setOnClickListener {
+                findNavController().navigate(R.id.action_homeFragment_to_alteVerseFragment)
+            }
+
+            logoutBtn.setOnClickListener {
+                alteViewModel.logOut { logOutSuccessful, errorMessage ->
+
+                    if (logOutSuccessful) {
+                        findNavController().navigate(R.id.action_homeFragment_to_signInFragment)
+
+                    } else {
+
+                        errorMessage?.let { message ->
+                            val output = message.substringAfter(": ")
+                            toastManager.showLongToast(requireContext(), output)
+                        }
+                    }
+                }
             }
         }
     }
 
-    private fun showProgress() {
-        // Set the animation file
-        customBar.setAnimation("progress_animation.json")
+    private fun showOverflowMenu(view: View) {
+        val popupMenu = PopupMenu(requireContext(), view)
 
-        // Start the animation
-        customBar.playAnimation()
+        // Inflate the menu resource
+        popupMenu.menuInflater.inflate(R.menu.home_overflow_menu, popupMenu.menu)
 
-        customBar.visibility = View.VISIBLE
+        // Set an OnMenuItemClickListener for the menu items
+        popupMenu.setOnMenuItemClickListener { item ->
+            when (item.itemId) {
+                R.id.new_circle -> {
+
+                    true
+                }
+
+                R.id.new_broadcast -> {
+                    true
+                }
+
+                R.id.starred_messages -> {
+                    true
+                }
+
+                R.id.settings -> {
+                    true
+                }
+
+                else -> false
+            }
+        }
+
+        // Show the popup menu
+        popupMenu.show()
+    }
+    fun convertResourceIdsToBitmaps(resourceIds: List<Int>): List<Bitmap> {
+        val bitmaps = mutableListOf<Bitmap>()
+
+        for (resourceId in resourceIds) {
+            val bitmap = BitmapFactory.decodeResource(resources, resourceId)
+            bitmaps.add(bitmap)
+        }
+
+        return bitmaps
     }
 }
