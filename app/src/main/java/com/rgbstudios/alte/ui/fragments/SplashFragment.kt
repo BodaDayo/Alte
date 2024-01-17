c834ccb9417cf7e8e09eb62e0ceb140e0909b5c6
Oladayo <oladayobb@gmail.com>
2024-01-17 18:57:18 +0100

    SplashFragment now set properly, navigates to appropriate fragment based on first-launch, signed in and userDetails-saved conditions

--- app/src/main/java/com/rgbstudios/alte/ui/fragments/SplashFragment.kt
+++ app/src/main/java/com/rgbstudios/alte/ui/fragments/SplashFragment.kt
@@ -11,17 +11,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.google.firebase.auth.FirebaseAuth
 import com.rgbstudios.alte.R
 import com.rgbstudios.alte.data.remote.FirebaseAccess
+import com.rgbstudios.alte.data.repository.AlteRepository
 import com.rgbstudios.alte.utils.ToastManager
+import com.rgbstudios.alte.viewmodel.AlteViewModel
+import com.rgbstudios.alte.viewmodel.AlteViewModelFactory
 
 class SplashFragment : Fragment() {
 
     private lateinit var auth: FirebaseAuth
     private var delayedNavigationHandler: Handler? = null
     private val toastManager = ToastManager()
+
+    private val firebase = FirebaseAccess()
+
+    private val alteViewModel: AlteViewModel by viewModels {
+        AlteViewModelFactory(requireActivity().application, AlteRepository(firebase))
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -55,33 +65,35 @@ class SplashFragment : Fragment() {
         delayedNavigationHandler = Handler(Looper.myLooper()!!)
 
         delayedNavigationHandler?.postDelayed(Runnable {
-            checkNetworkConnectivity()
 
-            // Navigate to OnboardingFragmentOne
-            findNavController().navigate(R.id.action_splashFragment_to_signUpFragment)
+            // Check if it's the first launch
+            val isFirstLaunch = alteViewModel.isFirstLaunch.value ?: true
 
+            if (isFirstLaunch) {
+
+                // If it's the first launch, navigate to OnboardingFragmentOne
+                findNavController().navigate(R.id.action_splashFragment_to_onboardingFragment)
+
+            } else {
+                // It's not first launch
+                if (auth.currentUser != null) {
+
+                    val isUsernameSet = alteViewModel.isUsernameSet.value ?: false
+
+                    if (isUsernameSet) {
+
+                        // If username is set, navigate to HomeFragment
+                        findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+
+                    } else {
+                        // Navigate to completeRegistrationFragment
+                        findNavController().navigate(R.id.action_splashFragment_to_completeRegistrationFragment)
+                    }
+                } else {
+
+                    findNavController().navigate(R.id.action_splashFragment_to_onboardingFinalFragment)
+                }
+            }
         }, 2000)
-    }
-
-    private fun checkNetworkConnectivity() {
-
-        // Get the ConnectivityManager instance
-        val connectivityManager =
-            requireContext().getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-
-        // Get the network capabilities
-        val networkCapabilities =
-            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
-
-        // Check if the network capabilities have internet access
-        if (networkCapabilities?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) == true) {
-            // There is internet connection
-        } else {
-            // There is no internet connection
-            toastManager.showLongToast(
-                requireContext(),
-                "No internet connection.\nSome features might be unavailable"
-            )
-        }
     }
 }
