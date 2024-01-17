f99d8f9a87400b8af152d9a2480cea1c80ff38ce
Oladayo <oladayobb@gmail.com>
2024-01-17 19:24:39 +0100

    Class to provide avatar assets in the app

---
+++ app/src/main/java/com/rgbstudios/alte/utils/AvatarManager.kt
@@ -0,0 +1,22 @@
+package com.rgbstudios.alte.utils
+
+import com.rgbstudios.alte.R
+
+class AvatarManager {
+    val defaultAvatarsList = listOf(
+        R.drawable.asset1,
+        R.drawable.asset2,
+        R.drawable.asset3,
+        R.drawable.asset4,
+        R.drawable.asset5,
+        R.drawable.asset6,
+        R.drawable.asset7,
+        R.drawable.asset8,
+        R.drawable.asset9,
+        R.drawable.asset10,
+        R.drawable.asset11,
+        R.drawable.asset12,
+    )
+    val defaultAvatar = defaultAvatarsList.random()
+
+}
